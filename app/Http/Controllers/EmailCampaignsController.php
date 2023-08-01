a0d220109cea870ae9d619192e2dd983bbdc97af
MDAnwar58 <anwar.saeed656@gmail.com>
2023-08-01 10:39:53 +0600

    Assignment_22

--- app/Http/Controllers/EmailCampaignsController.php
+++ app/Http/Controllers/EmailCampaignsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\EmailCampaignRequest;
+use App\Mail\MailCampagin;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class EmailCampaignsController extends Controller
 {
@@ -14,8 +16,9 @@ class EmailCampaignsController extends Controller
     }
     function campaign(EmailCampaignRequest $request)
     {
-        User::create($request->validated());
-
+        $user = User::create($request->validated());
+        // $user = $request->validated();
+        Mail::to($request->email)->send(new MailCampagin($user));
         return redirect()->route('email.campaign')->with('success', 'Your information send successfully!');
     }
 }
